cfee5c64a7f382d7ee90f9e65eac3fed899b64e2
Dalt Bunker <dalt@bunker>
2023-02-10 17:26:59 -0700

    added cors handling

--- src/main/java/com/climbing_log/ClimbingLogApplication.java
+++ src/main/java/com/climbing_log/ClimbingLogApplication.java
@@ -2,7 +2,10 @@ package com.climbing_log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
@@ -13,5 +16,17 @@ public class ClimbingLogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClimbingLogApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+					.allowedOrigins("http://localhost:4200", "https://climbinglogclient-production.up.railway.app")
+					.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 }
